24346b43943820c3fb78815ab5eda389d3ecf434
jan-hendrik.schulte <jan-hendrik.schulte@adesso.de>
2025-01-10 16:52:17 +0100

    updated config

--- include/Config.hh
+++ include/Config.hh
@@ -1,3 +1,7 @@
+// if defined, enables EXTERNAL_TEST_MODE:
+// simulating a real drone (disabled lockstepping, no quaternion and velocity, just magnetic field and attitude)
+// in addition using data bytes instead of a json string to transfer sensor values
+
 #define EXTERNAL_TEST_MODE
 
 #ifdef EXTERNAL_TEST_MODE
